3404ce87cc9653811f5f8e4ca45db16a6f2872bc
Lindsan <102190284+Lindsan@users.noreply.github.com>
2023-04-28 09:31:36 +1200

    Add files via upload

---
+++ Dijkstra/Dijkstra.java
@@ -0,0 +1,22 @@
+
+/**
+ * Dijkstra.
+ *
+ * Angus Lindsay
+ * 28/04/23
+ */
+public class Dijkstra
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Dijkstra
+     */
+    public Dijkstra()
+    {
+        
+    }
+
+    
+}
