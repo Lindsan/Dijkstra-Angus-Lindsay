cc9751ef348f0d3aff494b7341351079c822b26d
Lindsan <102190284+Lindsan@users.noreply.github.com>
2023-06-22 15:09:22 +1200

    Add files via upload
    Did testing and trialling changed the colours and sizes of nodes and text and lines in the GUI.

--- simple dijkstra/GUI.java
+++ simple dijkstra/GUI.java
@@ -9,6 +9,8 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 import java.util.Map;
+import java.awt.geom.Line2D;
+
 public class GUI extends JFrame {
     // Represents the GUI for displaying the graph.
 
@@ -58,40 +60,48 @@ public class GUI extends JFrame {
     }
 
     public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2 = (Graphics2D) g;
-        int circleSize = 30;
-        
+    super.paint(g);
+    Graphics2D g2 = (Graphics2D) g;
+    int circleSize = 60;
 
-        // Draws the nodes on the canvas.
-        for (Node node : graph.getNodes()) {
-            Random rand = new Random();
-            int x = rand.nextInt(1000) + 50;
-            int y = rand.nextInt(550) + 50;
-            node.setX(x);
-            node.setY(y);
-            g2.setColor(Color.BLACK);
-            g2.fillOval(x, y, circleSize, circleSize);
-            
-            g2.setColor(Color.RED);
-            g2.drawString(node.getName(), x + (circleSize / 2), y + (circleSize / 2));
-            
-        }
+    // Pre-calculate the positions of the nodes
+    for (Node node : graph.getNodes()) {
+        Random rand = new Random();
+        int x = rand.nextInt(1000) + 50;
+        int y = rand.nextInt(550) + 50;
+        node.setX(x);
+        node.setY(y);
+    }
 
-        // Draws the edges (lines) between connected nodes.
-        g2.setColor(Color.BLUE);
-        for (Node node : graph.getNodes()) {
-            int startX = node.getX() + (circleSize / 2);
-            int startY = node.getY() + (circleSize / 2);
-            for (Map.Entry<Node, Integer> entry : node.getAdjacentNodes().entrySet()) {
-                Node adjacentNode = entry.getKey();
-                int endX = adjacentNode.getX() + (circleSize / 2);
-                int endY = adjacentNode.getY() + (circleSize / 2);
-                g2.drawLine(startX, startY, endX, endY);
-            }
+    // Draws the edges (lines) between connected nodes.
+    g2.setColor(Color.BLACK);
+    for (Node node : graph.getNodes()) {
+        int startX = node.getX() + (circleSize / 2);
+        int startY = node.getY() + (circleSize / 2);
+        for (Map.Entry<Node, Integer> entry : node.getAdjacentNodes().entrySet()) {
+            Node adjacentNode = entry.getKey();
+            int endX = adjacentNode.getX() + (circleSize / 2);
+            int endY = adjacentNode.getY() + (circleSize / 2);
+            g2.setStroke(new BasicStroke(10));
+            g2.draw(new Line2D.Float(startX, startY, endX, endY));
         }
     }
+
+    // Draws the nodes on the canvas.
+    for (Node node : graph.getNodes()) {
+        g2.setColor(Color.BLACK);
+        int x = node.getX();
+        int y = node.getY();
+        g2.fillOval(x, y, circleSize, circleSize);
+
+        g2.setColor(Color.WHITE);
+        g2.drawString(node.getName(), x + 10, y + (circleSize / 2));
+    }
 }
+
+
+}
+
 
 
 
